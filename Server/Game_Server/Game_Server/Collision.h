7ba38fdd3d4e24a29893210634db21e346c71af8
Heo Jae Seong <t55300354@gmail.com>
2022-11-20 00:12:31 +0900

    #11-20 허재성 Game_Server [충돌]
    Game_Server
    충돌 시나리오 구현
    1. 모션 내용
    - 지형,미사일,트랩,부스터
    2. 바운딩 박스 생성
    3. case 별 애니메이션

--- Server/Game_Server/Game_Server/Collision.h
+++ Server/Game_Server/Game_Server/Collision.h
@@ -1,9 +1,33 @@
 #pragma once
-#include <math.h>
 #include "../../Common/MyVectors.h"
+#include "CalcMove.h"
+#include "Global.h"
+MyVector3D vec3;
 
-MyVector3D TerrainCollision(MyVector3D vec , float veclocity, float scarla);
+struct ITeminfo
+{
+	int  item_value;
+	bool Missile;
+	bool Trap;
+	bool Booster;
+};
 
-MyVector3D ITemBoxCollision(MyVector3D vec);
-MyVector3D MissileCollision(MyVector3D vec);
-MyVector3D TrapCollision(MyVector3D vec);
+struct BoundingBoxInfo
+{
+	BoundingOrientedBox ItemboxOOBB = BoundingOrientedBox(GetPosition(), XMFLOAT3(4.0, 4.0, 4.0), XMFLOAT4(0.0, 0.0, 0.0, 1.0));
+	BoundingOrientedBox MissileOOBB = BoundingOrientedBox(GetPosition(), XMFLOAT3(4.0, 4.0, 2.0), XMFLOAT4(0.0, 0.0, 0.0, 1.0));
+	BoundingOrientedBox TrapOOBB = BoundingOrientedBox(GetPosition(), XMFLOAT3(6.0, 6.0, 6.0), XMFLOAT4(0.0, 0.0, 0.0, 1.0));
+	BoundingOrientedBox PlayerOOBB = BoundingOrientedBox(GetPosition(), XMFLOAT3(6.0, 6.0, 6.0), XMFLOAT4(0.0, 0.0, 0.0, 1.0));
+
+};
+
+XMFLOAT3 GetPosition() { return XMFLOAT3(vec3.x, vec3.y, vec3.z); };
+
+void TerrainExitCollision(MyVector3D vec , float veclocity, float scarla);
+int ITemBoxCollision();
+void MissileCollision(MyVector3D vec, float scarla, float elapsedtime);
+void TrapCollision(MyVector3D vec);
+
+void BoosterAnimate(MyVector3D vec, float elapsedtime);
+
+void Animate(MyVector3D vec, float veclocity, float scarla, float elapsedtime);
--- Server/Game_Server/Game_Server/Global.h
+++ Server/Game_Server/Game_Server/Global.h
@@ -4,3 +4,21 @@
 #define BUFSIZE    512
 
 #define MOVE_SCALAR 1.5f
+
+#include <random>
+#include <math.h>
+#include <d3d12.h>
+#include <dxgi1_4.h>
+#include <D3Dcompiler.h>
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <DirectXCollision.h>
+#include <DirectXCollision.inl>
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "dxguid.lib")
+
+using namespace std;
+using namespace DirectX;
+using namespace DirectX::PackedVector;
